28d20fa63d35afdb635dca6be7be30b579e1f8ac
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-02-01 00:03:47 +0300

    Bootstrap

--- conanfile.py
+++ conanfile.py
@@ -9,11 +9,28 @@ class BoostConan(ConanFile):
     description = "Boost provides free peer-reviewed portable C++ source libraries"
     url = "https://github.com/odant/conan-boost"
     settings = "os", "compiler", "build_type", "arch"
-    boost_source_archive = "boost_%s.tar.gz" % version.replace(".", "_")
-    exports_sources = boost_source_archive, "FindBoost.cmake"
+    #------ internal ------
+    _boost_name = "boost_%s" % version.replace(".", "_")
+    _boost_archive = _boost_name + ".tar.gz"
+    #------ internal ------
+    exports_sources = _boost_archive, "FindBoost.cmake"
     no_copy_source = True
     build_policy = "missing"
 
     def source(self):
-        tools.unzip(self.boost_source_archive)
-        os.remove(self.boost_source_archive)
+        tools.unzip(self._boost_archive)
+        os.remove(self._boost_archive)
+        
+    def build(self):
+        b2 = self.bootstrap()
+        self.run("%s -v" % b2)
+        
+    def bootstrap(self):
+        boost_source = os.path.join(self.source_folder, self._boost_name)
+        is_windows = tools.os_info.is_windows
+        with tools.chdir(boost_source):
+            cmd = "bootstrap.bat" if is_windows else "./bootstrap.sh"
+            self.run(cmd)
+        b2_exe = "b2.exe" if is_windows else "b2"
+        return os.path.join(boost_source, b2_exe)
+
