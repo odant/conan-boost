3212ba7d999eb48296a9f1ffc69cb4d004b1144a
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-02-19 13:02:50 +0300

    Add test_package for boost::regex

---
+++ test_package/test_boost_regex.cpp
@@ -0,0 +1,19 @@
+#include <boost/regex.hpp>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+
+int main(int, char**) {
+    {
+        std::string s = "Test Boost::regex";
+        boost::regex re{R"rr(\w+\s\w+::\w+)rr"};
+        std::cout << std::boolalpha << boost::regex_match(s, re) << std::endl;
+        if(!boost::regex_match(s, re)) {
+            std::cout << "Error: boost::regex_match(s, re) is false " << __FILE__ << __LINE__ << std::endl;
+            std::exit(EXIT_FAILURE);
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
