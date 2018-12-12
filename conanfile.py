4298367af53248a8d633779c0fdc7b5ef328a583
d0nc1h0t <>
2018-12-12 16:28:51 +0300

    set BOOST_LOG_CXX11_CODECVT_FACETS_FORCE_ENABLE

--- conanfile.py
+++ conanfile.py
@@ -123,6 +123,9 @@ class BoostConan(ConanFile):
             "variant=%s" % str(self.settings.build_type).lower(),
             "address-model=%s" % {"x86": "32", "x86_64": "64", "mips": "32"}.get(str(self.settings.arch))
         ])
+		# add BOOST_LOG_CXX11_CODECVT_FACETS_FORCE_ENABLE
+        if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
+            flags.append("define=BOOST_LOG_CXX11_CODECVT_FACETS_FORCE_ENABLE")
         # locale use ICU
         icu_path = self.deps_cpp_info["icu"].rootpath.replace("\\", "/")
         icu_lib_path = self.deps_cpp_info["icu"].lib_paths[0]
@@ -265,6 +268,9 @@ class BoostConan(ConanFile):
         ]
         if self.settings.os == "Windows":
             self.cpp_info.defines.append("_WIN32_WINNT=0x0601") # 7 or Server 2008 R2
+		# add BOOST_LOG_CXX11_CODECVT_FACETS_FORCE_ENABLE
+        if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
+            self.cpp_info.defines.append("BOOST_LOG_CXX11_CODECVT_FACETS_FORCE_ENABLE")
         # Enable char16_t and char32_t
         if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
             pass
--- test_package/test_boost_log.cpp
+++ test_package/test_boost_log.cpp
@@ -1,22 +1,28 @@
 // Boost test for Conan package
 // Dmitriy Vetutnev, ODANT, 2018
 
+#ifdef BOOST_LOG_CXX11_CODECVT_FACETS_FORCE_ENABLE
+#    ifndef BOOST_LOG_TEST_DISABLE
+#        define BOOST_LOG_TEST_DISABLE
+#    endif
+#endif
 
-#include <boost/log/common.hpp>
-#include <boost/log/sinks.hpp>
-#include <boost/log/sources/logger.hpp>
-#include <boost/shared_ptr.hpp>
-#include <boost/core/null_deleter.hpp>
+#ifndef BOOST_LOG_TEST_DISABLE
+#    include <boost/log/common.hpp>
+#    include <boost/log/sinks.hpp>
+#    include <boost/log/sources/logger.hpp>
+#    include <boost/shared_ptr.hpp>
+#    include <boost/core/null_deleter.hpp>
+
+using Backend = boost::log::sinks::text_ostream_backend;
+using Sink = boost::log::sinks::synchronous_sink<Backend>;
+#endif
 
 #include <iostream>
 #include <cstdlib>
 
-
-using Backend = boost::log::sinks::text_ostream_backend;
-using Sink = boost::log::sinks::synchronous_sink<Backend>;
-
 int main(int, char**) {
-
+#ifndef BOOST_LOG_TEST_DISABLE
     boost::shared_ptr<std::ostream> stream{&std::cout, boost::null_deleter{}};
 
     auto backend = boost::make_shared<Backend>();
@@ -30,6 +36,9 @@ int main(int, char**) {
 
     boost::log::sources::logger lg;
     BOOST_LOG(lg) << "Probe Boost::log";
-
+#else
+    std::cout << "Boost::log test disable (BOOST_LOG_TEST_DISABLE or BOOST_LOG_CXX11_CODECVT_FACETS_FORCE_ENABLE defined)" << std::endl;
+#endif
     return EXIT_SUCCESS;
+
 }
