c49d65eacacae49e21a032ca5276d9d8ff2d8dd0
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2020-04-30 06:17:23 +0300

    Update CI

--- build.py
+++ build.py
@@ -1,26 +1,23 @@
-# Boost Conan package
-# Dmitriy Vetutnev, ODANT, 2018-2020
+# Build Conan package
+# Dmitriy Vetutnev, ODANT, 2020
 
 
-import os
 from conan.packager import ConanMultiPackager
+import os
 
 
-# Common settings
-username = "odant" if "CONAN_USERNAME" not in os.environ else None
-# Windows settings
-visual_versions = ["15", "16"] if "CONAN_VISUAL_VERSIONS" not in os.environ else None
-visual_runtimes = ["MD", "MDd"] if "CONAN_VISUAL_RUNTIMES" not in os.environ else None
+env_pure_c = os.getenv("CONAN_PURE_C", True)
+pure_c = True if str(env_pure_c).lower() != "false" else False
 
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(
-        username=username,
-        visual_versions=visual_versions,
-        visual_runtimes=visual_runtimes,
         exclude_vcvars_precommand=True
     )
-    builder.add_common_builds(pure_c=False)
-    builder.remove_build_if(lambda build: build.settings.get("compiler.libcxx") == "libstdc++")
+    builder.add_common_builds(
+        pure_c=pure_c
+    )
+    builder.remove_build_if(
+        lambda build: build.settings.get("compiler.libcxx") == "libstdc++"
+    )
     builder.run()
-
