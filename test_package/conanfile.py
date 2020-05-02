29c6c7d1c2571a0d4dce8b40f87941e0a7fb6ca8
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2020-05-03 02:24:24 +0300

    Build tests with Ninja

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,5 +1,5 @@
 # Boost test for Conan package
-# Dmitriy Vetutnev, Odant, 2018
+# Dmitriy Vetutnev, Odant, 2018, 2020
 
 
 from conans import ConanFile, CMake, tools
@@ -8,6 +8,7 @@ from conans import ConanFile, CMake, tools
 class PackageTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
+    requires = "ninja/1.9.0"
 
     def imports(self):
         self.copy("*.pdb", dst="bin", src="bin")
@@ -15,17 +16,19 @@ class PackageTestConan(ConanFile):
         self.copy("*.so*", dst="bin", src="lib")
 
     def build(self):
-        cmake = CMake(self, msbuild_verbosity='normal')
+        cmake = CMake(self, generator="Ninja", msbuild_verbosity='normal')
         cmake.verbose = True
         cmake.configure()
         cmake.build()
+        self.cmake_is_multi_configuration = cmake.is_multi_configuration
 
     def test(self):
         env = {}
         if self.settings.os != "Windows":
             env = {"LANG": "en_US.utf8"}
         with tools.environment_append(env):
-            if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
+            if self.cmake_is_multi_configuration:
                 self.run("ctest --verbose --build-config %s" % self.settings.build_type)
             else:
                 self.run("ctest --verbose")
+
