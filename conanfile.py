0410941b3f43c9162ae9c6a58770105fe4a19e5d
d0nc1h0t <udincev@yandex.ru>
2025-09-11 18:29:53 +0300

    Version 1.89.0+0

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ import os, platform, re
 
 class BoostConan(ConanFile):
     name = "boost"
-    version = "1.88.0+1"
+    version = "1.89.0+0"
     license = "Boost Software License - Version 1.0. http://www.boost.org/LICENSE_1_0.txt"
     description = "Boost provides free peer-reviewed portable C++ source libraries"
     url = "https://github.com/odant/conan-boost"
